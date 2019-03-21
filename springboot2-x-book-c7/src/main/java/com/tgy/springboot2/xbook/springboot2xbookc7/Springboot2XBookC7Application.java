21dd6c705c81c2caa65ab1b64af7959b3671cf97
tianguiyin <617755200@qq.com>
2019-03-22 01:43:46 +0800

    提交7章-redis

---
+++ springboot2-x-book-c7/src/main/java/com/tgy/springboot2/xbook/springboot2xbookc7/Springboot2XBookC7Application.java
@@ -0,0 +1,13 @@
+package com.tgy.springboot2.xbook.springboot2xbookc7;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot2XBookC7Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Springboot2XBookC7Application.class, args);
+    }
+
+}
