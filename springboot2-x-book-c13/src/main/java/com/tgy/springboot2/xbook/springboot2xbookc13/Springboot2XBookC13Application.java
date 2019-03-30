5b7c0c16135e411d1b5d6fc680a5987ae9951b1e
tianguiyin <617755200@qq.com>
2019-03-30 16:38:35 +0800

    提交13章

---
+++ springboot2-x-book-c13/src/main/java/com/tgy/springboot2/xbook/springboot2xbookc13/Springboot2XBookC13Application.java
@@ -0,0 +1,13 @@
+package com.tgy.springboot2.xbook.springboot2xbookc13;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot2XBookC13Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Springboot2XBookC13Application.class, args);
+    }
+
+}
